dbdde322812affdacbbe266ce03f2e895e20eb27
AlanPerezD <52674692+AlanPerezD@users.noreply.github.com>
2023-02-02 08:22:58 -0300

    added client

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const HeroBanner = ({heroBanner}) => {
+import { urlFor } from '@/lib/client'
+
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
         <p className='auriculares-hyperx-cloud'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.LargeText1}</h1>
-        <img src={urlFor} />
+        <img src={urlFor(heroBanner.image)} />
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
-export default function Home({bannerData}) {
+export default function Home({bannerData, products}) {
   return (
     <div>
       <HeroBanner heroBanner={bannerData} />
